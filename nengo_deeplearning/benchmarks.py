d56080da8cfaf9d53c19159ccbe3af9020ee0d84
drasmuss <daniel.rasmussen@appliedbrainresearch.com>
2016-12-20 13:24:35 -0500

    Update benchmarks

--- nengo_deeplearning/benchmarks.py
+++ nengo_deeplearning/benchmarks.py
@@ -68,44 +68,58 @@ def compare_backends():
     neuron_types = [nengo.RectifiedLinear]  # , nengo.LIF]
     backends = [nengo_dl, nengo, nengo_ocl]
 
-    data = np.zeros((len(benchmarks), len(n_range), len(d_range),
-                     len(neuron_types), len(backends)))
+    # data = np.zeros((len(benchmarks), len(n_range), len(d_range),
+    #                  len(neuron_types), len(backends)))
+    #
+    # for i, bench in enumerate(benchmarks):
+    #     for j, neurons in enumerate(n_range):
+    #         for k, dimensions in enumerate(d_range):
+    #             for l, neuron_type in enumerate(neuron_types):
+    #                 print("-" * 30)
+    #                 print(bench, neurons, dimensions, neuron_type)
+    #
+    #                 net = bench(dimensions, neurons, neuron_type())
+    #                 model = nengo.builder.Model()
+    #                 model.build(net)
+    #
+    #                 for m, backend in enumerate(backends):
+    #                     print(backend)
+    #
+    #                     try:
+    #                         with backend.Simulator(None, model=model) as sim:
+    #                             start = time.time()
+    #                             sim.run(5.0)
+    #                             data[i, j, k, l, m] = time.time() - start
+    #                     except Exception:
+    #                         print(backend, "crashed")
+    #                         data[i, j, k, l, m] = np.nan
+    #
+    # np.savez("benchmark_data.npz", data)
 
-    for i, bench in enumerate(benchmarks):
-        for j, neurons in enumerate(n_range):
-            for k, dimensions in enumerate(d_range):
-                for l, neuron_type in enumerate(neuron_types):
-                    print("-" * 30)
-                    print(bench, neurons, dimensions, neuron_type)
+    data = np.load("benchmark_data.npz")["arr_0"]
 
-                    net = bench(dimensions, neurons, neuron_type())
-                    model = nengo.builder.Model()
-                    model.build(net)
-
-                    for m, backend in enumerate(backends):
-                        print(backend)
-
-                        with backend.Simulator(None, model=model) as sim:
-                            start = time.time()
-                            sim.run(5.0)
-                            data[i, j, k, l, m] = time.time() - start
-    np.savez("benchmark_data.npz", data)
-
-    # data = np.load("benchmark_data.npz")["arr_0"]
-
+    bench_names = ["pes", "cconv", "integrator"]
+    neuron_names = ["relu"]
     for i in range(len(benchmarks)):
         for j in range(len(neuron_types)):
             plt.figure()
-            plt.title("%s %s" % (benchmarks[i], neuron_types[j]))
+            plt.title("%s (%s)" % (bench_names[i], neuron_names[j]))
             plt.plot(d_range, data[i, 0, :, j, 0] / data[i, 0, :, j, 1])
             plt.xlabel("dimensions")
             plt.ylabel("nengo_dl / nengo")
 
             plt.figure()
-            plt.title("%s %s" % (benchmarks[i], neuron_types[j]))
+            plt.title("%s (%s)" % (bench_names[i], neuron_names[j]))
             plt.plot(d_range, data[i, 0, :, j, 0] / data[i, 0, :, j, 2])
             plt.xlabel("dimensions")
             plt.ylabel("nengo_dl / nengo_ocl")
+
+            plt.figure()
+            plt.title("%s (%s)" % (bench_names[i], neuron_names[j]))
+            plt.plot(d_range, data[i, 0, :, j, :])
+            plt.xlabel("dimensions")
+            plt.ylabel("seconds")
+            plt.legend(["nengo_dl", "nengo", "nengo_ocl"])
 
     plt.show()
 
@@ -120,5 +134,5 @@ def profiling():
 
 
 if __name__ == "__main__":
-    # compare_backends()
-    profiling()
+    compare_backends()
+    # profiling()
