512affd0b52a769f12e24a39f0d9776092b69b58
Daniel Rasmussen <daniel.rasmussen@appliedbrainresearch.com>
2018-10-10 14:09:00 -0300

    Log run_checks output to file

--- .ci/run_checks.bat
+++ .ci/run_checks.bat
@@ -1,16 +1,15 @@
 set DEVICE=%1
-
 set DIR=%cd%
 set PYOPENCL_CTX=0
 
-pylint ../nengo_dl --rcfile=../setup.cfg
-pytest --pyargs nengo --device=%DEVICE% --dtype=float32 --unroll_simulation=1 || goto :exit
-pytest ../nengo_dl --device=%DEVICE% --dtype=float32 --unroll_simulation=1 || goto :exit
-python ../nengo_dl/benchmarks.py performance_samples --device %DEVICE% || goto :exit
+pylint ../nengo_dl --rcfile=../setup.cfg > ../tmp/run_checks.txt 2>&1 || goto :exit
+pytest --pyargs nengo --device=%DEVICE% --dtype=float32 --unroll_simulation=1 >> ../tmp/run_checks.txt 2>&1 || goto :exit
+pytest ../nengo_dl --device=%DEVICE% --dtype=float32 --unroll_simulation=1 >> ../tmp/run_checks.txt 2>&1 || goto :exit
+python ../nengo_dl/benchmarks.py performance_samples --device %DEVICE% >> ../tmp/run_checks.txt 2>&1 || goto :exit
 
-cd %TEMP%
-python %DIR%/../docs/whitepaper/whitepaper2018_plots.py --no-show --reps 1 test || goto :exit
+cd /d %TEMP%
+python %DIR%/../docs/whitepaper/whitepaper2018_plots.py --no-show --reps 1 test >> ../tmp/run_checks.txt 2>&1 || goto :exit
 
 :exit
-  cd %DIR%
+  cd /d %DIR%
   exit /b %errorlevel%
