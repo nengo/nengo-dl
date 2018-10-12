20d3d8ff3306d353168658baee16f0e3c379a4cf
Daniel Rasmussen <daniel.rasmussen@appliedbrainresearch.com>
2018-10-12 14:28:43 -0300

    Combine coverage with version tests

--- .ci/coverage.sh
+++ .ci/coverage.sh
@@ -2,8 +2,8 @@
 set -e -v
 
 if [[ $1 == "script" ]]; then
-  coverage run -m pytest $TEST_ARGS --pyargs nengo
-  coverage run -a -m pytest $TEST_ARGS nengo_dl
+  coverage run -m pytest $TEST_ARGS -v --color=yes --pyargs nengo
+  coverage run -a -m pytest $TEST_ARGS -v --color=yes --durations 20 nengo_dl
   coverage report -m
 elif [[ $1 == "after_success" ]]; then
   eval "bash <(curl -s https://codecov.io/bash)"
