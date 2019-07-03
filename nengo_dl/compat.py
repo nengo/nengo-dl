3079268e31726202a40310e04b00c6d6a054d55e
Daniel Rasmussen <daniel.rasmussen@appliedbrainresearch.com>
2019-07-03 12:04:12 -0300

    Remove unused TensorFlow backwards compatibility

--- nengo_dl/compat.py
+++ nengo_dl/compat.py
@@ -10,44 +10,14 @@ from distutils.version import LooseVersion
 import nengo
 import tensorflow as tf
 
-if LooseVersion(tf.__version__) < LooseVersion("2.0.0"):
-    tf_compat = tf
 
-    def tf_convolution(*args, **kwargs):
-        """Convert "filters" kwarg to "filter"."""
+tf_compat = tf.compat.v1
 
-        if "filters" in kwargs:
-            kwargs["filter"] = kwargs.pop("filters")
-        return tf.nn.convolution(*args, **kwargs)
 
-    tf_math = tf
+def RefVariable(*args, **kwargs):
+    """Always returns RefVariables instead of (maybe) ResourceVariables."""
 
-    def tf_shape(shape):
-        """Convert ``tf.Dimension`` to int."""
-
-        return tuple(x.value for x in shape)
-
-    tf_uniform = tf.random_uniform
-
-    RefVariable = tf.Variable
-else:
-    tf_compat = tf.compat.v1
-
-    tf_convolution = tf.nn.convolution
-
-    tf_math = tf.math
-
-    def tf_shape(shape):
-        """Return shape (elements are already ints)."""
-
-        return shape
-
-    tf_uniform = tf.random.uniform
-
-    def RefVariable(*args, **kwargs):
-        """Always returns RefVariables instead of (maybe) ResourceVariables."""
-
-        return tf.compat.v1.Variable(*args, use_resource=False, **kwargs)
+    return tf.compat.v1.Variable(*args, use_resource=False, **kwargs)
 
 
 if LooseVersion(nengo.__version__) < "3.0.0":
